ad216fa98dc68a1c395aeb2d36db9cee52116e20
YellowFish <imyellowfish@hotmail.com>
2017-05-03 16:10:07 +0800

    upload main project

---
+++ Dispatcher.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Dispatcher<T> where T : System.IConvertible{
+    public bool debug = false;
+    public Dispatcher(bool debug) {
+        this.debug = debug;
+    }
+
+    public delegate void EventHandler(params object[] args);
+
+    private Dictionary<object, EventHandler> dict = new Dictionary<object, EventHandler>();
+
+    public void AddListener(T key, EventHandler handler) {
+        if (dict.ContainsKey(key)) {
+            dict[key] += handler;
+        } else {
+            dict.Add(key, handler);
+        }
+    }
+
+    public void RemoveListener(T key, EventHandler handler) {
+        if (dict.ContainsKey(key)) {
+            dict[key] -= handler;
+        } else {
+            throw new System.ArgumentException("Event to remove does not exist: " + key);  // TODO throw
+        }
+    }
+
+    public void Dispatch(T key, params object[] args) {
+        if (debug)
+            Debug.Log("dispatch event: " + key);
+
+        if (!dict.ContainsKey(key) || dict[key] == null) {
+            return;
+        }
+
+        dict[key].Invoke(args);
+    }
+}
---
+++ TestDispatcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestDispatcher : MonoBehaviour {
+    public enum Events { Start = 0, End = 1}
+    private Dispatcher<Events> dispatcher = new Dispatcher<Events>(true);
+
+	// Use this for initialization
+	void Start () {
+        dispatcher.AddListener(Events.Start, (v) => { Debug.Log("on start: " + v[0]); });
+        dispatcher.AddListener(Events.Start, (v) => { Debug.Log("on start no param: "); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            dispatcher.Dispatch(Events.Start, 0);
+        }
+	}
+}
