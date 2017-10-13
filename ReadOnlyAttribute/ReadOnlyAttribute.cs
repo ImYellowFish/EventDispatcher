dbe7888a92543ea24ff5ec3ce61fdbb8f194087a
YellowFish <imyellowfish@hotmail.com>
2017-10-13 15:53:06 +0800

    add: read only attributes for unity inspector

---
+++ ReadOnlyAttribute/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+﻿// This code is quoted from it3ration's answer in Unity3D forum
+// http://answers.unity3d.com/questions/489942/how-to-make-a-readonly-property-in-inspector.html
+//
+
+using UnityEngine;
+public class ReadOnlyAttribute : PropertyAttribute {
+
+}
